c7fc32420abf2a9c8a8c0a84a3efae9485c0c438
matsjoonas <mj@codelight.eu>
2019-03-07 15:03:23 +0200

    Add icon template and matching css styles

---
+++ resources/views/partials/icon.blade.php
@@ -0,0 +1,5 @@
+<div class="ScalingSvg ScalingSvg-{{ $name }}">
+  <svg class="ScalingSvg_shape">
+    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-{{ $name }}"></use>
+  </svg>
+</div>
