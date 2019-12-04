e3a86912034229e49a37ac3d792a1c5018194c4e
Steven Zimdin <zimdin12@msn.com>
2019-12-04 15:06:27 +0200

    File for production scripts (only loaded on production)

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  @if(WP_ENV === 'production')
+	  @include('partials.production-scripts')
+  @endif
   @php wp_head() @endphp
 </head>
