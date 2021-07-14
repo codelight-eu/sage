b53c754bbf4cfa6a1c4848412be138d455b9284f
Indrek Kaine <indrek.kaine@codelight.eu>
2021-07-14 13:44:45 +0300

    Add style to mobile menu

--- resources/views/partials/main-menu-mobile.blade.php
+++ resources/views/partials/main-menu-mobile.blade.php
@@ -1,4 +1,4 @@
-<nav class="MainMenuMobile js-collapseSubs">
+<nav class="MainMenuMobile">
   @if (has_nav_menu('main_navigation_mobile'))
     {!! wp_nav_menu(['theme_location' => 'main_navigation_mobile', 'menu_class' => 'MainMenuMobile_list', 'container' => false]) !!}
   @endif
