0926968f7f097ccdc04e4653360b84318f895398
Indrek Kaine <indrek.kaine@codelight.eu>
2021-07-14 15:42:09 +0300

    Make CreatorBrand clickable

--- resources/views/partials/creator-brand.blade.php
+++ resources/views/partials/creator-brand.blade.php
@@ -1,5 +1,8 @@
+@php $creatorBrandLink = 'https://codelight.eu'; @endphp
 <div class="CreatorBrand">
-  <div class="CreatorBrand_icon">
-    @include("partials.icon", ["name" => 'creator-logo'])
-  </div>
+  @if($creatorBrandLink) <a href="{{ $creatorBrandLink }}" target="_blank" class="CreatorBrand_link"> @endif
+    <div class="CreatorBrand_icon">
+      @include("partials.icon", ["name" => 'creator-logo'])
+    </div>
+  @if($creatorBrandLink) </a> @endif
 </div>
