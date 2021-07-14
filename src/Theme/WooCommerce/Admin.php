12fda00b94223d84e45c63691411b121fa872091
Indrek Kõnnussaar <indrek@codelight.eu>
2021-07-14 16:21:27 +0300

    Add order complete url link to order admin

--- src/Theme/WooCommerce/Admin.php
+++ src/Theme/WooCommerce/Admin.php
@@ -15,9 +15,25 @@ class Admin
      */
     public function __construct()
     {
+        add_action('woocommerce_admin_order_data_after_order_details', [$this, 'renderOrderCompletedPageLink']);
         // add_filter('product_type_selector', [$this, 'removeProductTypes']);
         // add_filter('product_type_options', [$this, 'removeVirtualDownloadableProducts']);
         // add_filter('woocommerce_product_data_tabs', [$this, 'removeProductTabs']);
+    }
+
+    /**
+     * Show a link to order completed page in each order's admin view for easier development.
+     *
+     * @param \WC_Order $order
+     */
+    public function renderOrderCompletedPageLink(\WC_Order $order)
+    {
+        if (!in_array($order->get_status(), ['processing', 'completed'])) {
+            return;
+        }
+
+        $url = $order->get_checkout_order_received_url();
+        echo "<a target='_blank' style='display: inline-block; margin-top: 10px;' href='{$url}'>View order complete page</a>";
     }
 
     /**
